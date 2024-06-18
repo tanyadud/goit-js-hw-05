b882966551e7400d468e2be483ea0612624f4a61
Tanya Olieinykova <dp210795dtj1@gmail.com>
2024-06-18 15:53:21 +0300

    add four tasks( using reduce,toSorted, filter, includes, map)

---
+++ js/task-4.js
@@ -0,0 +1,51 @@
+"use srict";
+
+const getTotalBalanceByGender = (users, gender) => {
+    return users.reduce((totalBalance, client) => {
+        if (client.gender === gender) {
+          return totalBalance + client.balance;
+        }
+        return totalBalance;
+      }, 0)
+};
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male"));
+console.log(getTotalBalanceByGender(clients, "female")); 
